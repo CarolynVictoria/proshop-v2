d946d91504c9c2e7d8e0acf15b027039de6e7901
cvmgo <design@newlightarts.com>
2023-12-17 14:23:15 -0500

    through product detail screen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -11,13 +11,13 @@ const Product = ({ product }) => {
 
 			<Card.Body>
 				<Link to={`/product/${product._id}`}>
-					<Card.Title as="div">
+					<Card.Title as="div" className="product-title">
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</Link>
 				<Card.Text as="div">
 					<Rating
-						value={product.Rating}
+						value={product.rating}
 						text={`${product.numReviews} reviews`}
 					/>
 				</Card.Text>
