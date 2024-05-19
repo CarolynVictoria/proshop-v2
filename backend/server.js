1cfe0772250fe2df44896e4aa7e1242f24c387c7
cvmgo <design@newlightarts.com>
2024-05-19 09:18:39 -0400

    AfterProdBuildMay-19-Morning

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 // Enables access to request cookies
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-	res.send('API is running.');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -38,8 +34,22 @@ app.get('/api/config/paypal', (req, res) =>
 	res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve(); //	Set __dirname to the current directory
+const __dirname = path.resolve(); //	Set to the current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+	// set the react build folder  - static	folder
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	// any route that is not an API route will load the index.html file
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running.');
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
